594826c9b2c1136c019b913a7288e0567e012397
Frankwillians <fkffrannk02@gmail.com>
2024-03-01 19:48:07 -0300

    consertando exercicio

--- src/application/alturas.java
+++ src/application/alturas.java
@@ -12,11 +12,12 @@ public class alturas {
         Pessoa pessoa;
         System.out.println("Quantas pessoas irão ser inseridas ? ");
         int n = sc.nextInt();
+
         double[] vect = new double[n];
         String[] vectNames = new String[n];
         double sum_height = 0.0;
-        double percent_young = 0;
         int young = 0;
+        double percent_young = (young / n) * 100;
         for (int i = 0 ; i < vect.length ; i++){
             System.out.printf("Dados da %d ° pessoa: ", i+1);
             System.out.println();
@@ -29,23 +30,28 @@ public class alturas {
             double altura = sc.nextDouble();
             pessoa = new Pessoa(name,idade,altura);
             sum_height += pessoa.getHeight();
-            if (pessoa.getAge() <= 16){
-                vectNames[i] += pessoa.getName();
-                young += 1;
+            if (pessoa.getAge() < 16){
+                young = young + 1;
+                vectNames[i] = pessoa.getName();
+
 
             } 
-            percent_young = (young / n )* 100;
 
 
         }
-        System.out.println();
+
+
+        System.out.println(young);
         double height_avg = sum_height / vect.length;
         System.out.print("Altura media: " + String.format("%.2f",height_avg));
         System.out.println();
         System.out.print("Pessoas com menos de 16 anos: " + percent_young + "%");
         System.out.println();
-        for(int i = 0 ; i < vectNames.length ; i++){
-            System.out.println(vectNames[i]);
+        for(int i = 0 ; i < n ; i++){
+           
+            if(vectNames[i] != null ){
+                System.out.println(vectNames[i]);
+            }
         }
 
         sc.close();
